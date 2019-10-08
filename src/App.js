afcb7c6ef5df3233e74a967dae0824efd30b6237
github username <github email address>
2019-10-08 11:47:48 -0400

    format update

--- src/App.js
+++ src/App.js
@@ -19,13 +19,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" >
+            Learn React LifeCycle
+          </a>
+        </header>
 
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer" >
-          Learn React LifeCycle
-        </a>
-      </header>
 
         <Name firstName={this.state.name} />
         <Lang />
