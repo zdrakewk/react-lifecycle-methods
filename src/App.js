70a2b2d7e5dee2ad7834fa16e44aa17a99d0dbe3
github username <github email address>
2019-10-13 15:47:00 -0400

    add triangle component to test recursive component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import chart from './lifecyclechart.jpeg';
 import Name from './name';
 import Lang from './lang';
 import RecursiveColor from './components/recursiveColor';
+import Triangle from './components/triangle';
 
 
 class App extends Component {
@@ -26,6 +27,7 @@ class App extends Component {
           */}
 
         <RecursiveColor opacity={0.5}/>
+        <Triangle n={10}/>
 
         <Lang />
         <br/>
